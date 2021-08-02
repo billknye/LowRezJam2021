8c8818cf5fc07c37dda28e1b75a6530fe4348938
Ian Rivard <irivard@protk.com>
2021-08-02 11:43:45 -0400

    do some part of a particle system

--- src/LowRezJam2021/Game1.cs
+++ src/LowRezJam2021/Game1.cs
@@ -11,6 +11,10 @@ namespace LowRezJam2021
         private SpriteBatch _spriteBatch;
         private RenderTarget2D renderTarget;
         private SpriteFont font;
+        private Texture2D spaceTiles;
+        private float angle;
+        private bool pixelated;
+        private KeyboardState keyboardState;
 
         public Game1()
         {
@@ -32,6 +36,7 @@ namespace LowRezJam2021
             _spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>("Font");
             renderTarget = new RenderTarget2D(GraphicsDevice, 64, 64);
+            spaceTiles = Content.Load<Texture2D>("SpaceTiles");
         }
 
         protected override void Update(GameTime gameTime)
@@ -39,7 +44,16 @@ namespace LowRezJam2021
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            var mouseState = Mouse.GetState();
+            var keyboardStateLast = keyboardState;
+            keyboardState = Keyboard.GetState();
+
+            angle = MathF.Atan2(mouseState.Y - Window.ClientBounds.Height / 2, mouseState.X - Window.ClientBounds.Width / 2);
+
+            if (keyboardState.IsKeyDown(Keys.F2) && keyboardStateLast.IsKeyUp(Keys.F2))
+            {
+                pixelated = !pixelated;
+            }
 
             base.Update(gameTime);
         }
@@ -48,8 +62,13 @@ namespace LowRezJam2021
         {
             GraphicsDevice.SetRenderTarget(renderTarget);
             GraphicsDevice.Clear(Color.Black);
-            _spriteBatch.Begin();
+            _spriteBatch.Begin(samplerState: pixelated ? SamplerState.PointClamp : SamplerState.AnisotropicClamp);
+
+
             _spriteBatch.DrawString(font, "LowRezJam2021!!!", new Vector2(5, 5), Color.Teal);
+
+            _spriteBatch.Draw(spaceTiles, new Vector2(32, 32), new Rectangle(0, 0, 8, 8), Color.White, angle, new Vector2(4, 4), 1.0f, SpriteEffects.None, 0f);
+
             _spriteBatch.End();
 
             int scale = (int)Math.Floor(Math.Min(Window.ClientBounds.Width, Window.ClientBounds.Height) / 64.0f);
@@ -61,6 +80,73 @@ namespace LowRezJam2021
             _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+    }
+
+    public class Particle
+    {
+        public Color StartColor;
+        public Color EndColor;
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public float Created;
+    }
+
+    public class RingBuffer<T> where T : new()
+    {
+        T[] items;
+        int nextFree;
+
+        public RingBuffer(int count)
+        {
+            items = new T[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                items[i] = new T();
+            }
+        }
+
+        protected void Add(Action<T> change)
+        {
+            change(items[nextFree]);
+            nextFree = (nextFree + 1) % items.Length;
+        }
+    }
+
+    public class ParticleSet : RingBuffer<Particle>
+    {
+        int minScanIndex;
+        float currentTime;
+        float maxScanTime;
+
+        public ParticleSet(int count, float lifeTime)
+            : base(count)
+        {
+            LifeTime = lifeTime;
+        }
+
+        public float LifeTime { get; }
+
+        public void Add(Action<Particle> change)
+        {
+            base.Add(n =>
+            {
+                change(n);
+                n.Created = currentTime;
+            });
+
+            maxScanTime = currentTime + LifeTime;
+        }
+
+        public void Scan(float time)
+        {
+            currentTime = time;
+
+            while (true)
+            {
+
+            }
         }
     }
 }
